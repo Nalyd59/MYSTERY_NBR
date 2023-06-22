08f2d634159b0ac53d843b3cebd8fcaa0eb54ed6
Nalyd59 <dy.crepin@gmail.com>
2023-06-22 15:08:13 +0200

    set_lvl

--- script.js
+++ script.js
@@ -1,25 +1,121 @@
 i = 0;
 number_win = Math.floor(Math.random() * 10);
 
-function verif(number_test) {
-
-    if (number_test == number_win) {
-        document.getElementById('result').textContent = ' gagné !!';
-        i++;
-    }else{
-        document.getElementById('result').textContent = ' perdu !!';
-        i++;
-    }
-
-    switch (i) {
+function verif(number_test){
+    niv = document.getElementById('select').value;
+    switch (niv) {
         case 1:
-            document.getElementById('result_1').textContent = number_test;
+            alert('lvl 1');
+            essai = 3;
+            if (number_test == number_win) {
+                document.getElementById('ecran').textContent = ' gagné !! Le nombre est bien ' + number_win;
+                document.getElementById('container').style.background = '#14c214';
+                document.getElementById('header').style.background = 'grey';
+                document.getElementById('ecran').style.background = 'grey';
+                document.getElementById('footer').style.background = 'grey';
+                document.getElementById('nbr_ext').style.display = "none";
+                document.getElementById('input').style.display = "none";
+            }else{
+                document.getElementById('result').textContent = ' perdu !!';
+                i++;
+            }
+            switch (i) {
+                case 1:
+                    document.getElementById('result_1').textContent = number_test;
+                    break;
+                case 2:
+                    document.getElementById('result_2').textContent = number_test;
+                    break;
+                case 3:
+                    document.getElementById('result_3').textContent = number_test;
+                    break;
+            }
+            if (i > essai) {
+                document.getElementById('ecran').style.display = "none";
+                document.getElementById('input').style.display = "none";
+                window.location.reload();
+            }
+            if(i > (essai-1)){
+                document.getElementById('btn').value = "RECOMMENCER";
+                document.getElementById('nbr_ext').style.display = "none";
+            }
             break;
+
         case 2:
-            document.getElementById('result_2').textContent = number_test;
+            alert('lvl 2');
+            essai = 2;
+            if (number_test == number_win) {
+                document.getElementById('ecran').textContent = ' gagné !! Le nombre est bien ' + number_win;
+                document.getElementById('container').style.background = '#14c214';
+                document.getElementById('header').style.background = 'grey';
+                document.getElementById('ecran').style.background = 'grey';
+                document.getElementById('footer').style.background = 'grey';
+                document.getElementById('nbr_ext').style.display = "none";
+                document.getElementById('input').style.display = "none";
+            }else{
+                document.getElementById('result').textContent = ' perdu !!';
+                i++;
+            }
+            switch (i) {
+                case 1:
+                    document.getElementById('result_1').textContent = number_test;
+                    break;
+                case 2:
+                    document.getElementById('result_2').textContent = number_test;
+                    break;
+                case 3:
+                    document.getElementById('result_3').textContent = number_test;
+                    break;
+            }
+            if (i > essai) {
+                document.getElementById('ecran').style.display = "none";
+                document.getElementById('input').style.display = "none";
+                window.location.reload();
+            }
+            if(i > (essai-1)){
+                document.getElementById('btn').value = "RECOMMENCER";
+                document.getElementById('nbr_ext').style.display = "none";
+            }
             break;
+        
         case 3:
-            document.getElementById('result_3').textContent = number_test;
+            alert('lvl 3');
+            essai = 1;
+            if (number_test == number_win) {
+                document.getElementById('ecran').textContent = ' gagné !! Le nombre est bien ' + number_win;
+                document.getElementById('container').style.background = '#14c214';
+                document.getElementById('header').style.background = 'grey';
+                document.getElementById('ecran').style.background = 'grey';
+                document.getElementById('footer').style.background = 'grey';
+                document.getElementById('nbr_ext').style.display = "none";
+                document.getElementById('input').style.display = "none";
+            }else{
+                document.getElementById('result').textContent = ' perdu !!';
+                i++;
+            }
+            switch (i) {
+                case 1:
+                    document.getElementById('result_1').textContent = number_test;
+                    break;
+                case 2:
+                    document.getElementById('result_2').textContent = number_test;
+                    break;
+                case 3:
+                    document.getElementById('result_3').textContent = number_test;
+                    break;
+            }
+            if (i > essai) {
+                document.getElementById('ecran').style.display = "none";
+                document.getElementById('input').style.display = "none";
+                window.location.reload();
+            }
+            if(i > (essai-1)){
+                document.getElementById('btn').value = "RECOMMENCER";
+                document.getElementById('nbr_ext').style.display = "none";
+            }
+            break
+
+        default:
             break;
     }
 }
